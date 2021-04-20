af0ca38d9985e7541a43fa02573b228d75f3fc87
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-04-20 20:50:24 +0100

    Finished Task 2

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
-let playerMove = "scissors";
-let computerMove = "rock";
+// let playerMove = "scissors";
+// let computerMove = "rock";
 
-if (playerMove == "rock" && computerMove == "paper") {
-  console.log("Player Wins!");
-} else if (playerMove == "rock" && computerMove == "scissors") {
-  console.log("Computer Wins!");
-} else if (playerMove == "rock" && computerMove == "rock") {
-  console.log("Even -- Again!");
-} else if (playerMove == "paper" && computerMove == "rock") {
-  console.log("Player Wins!");
-} else if (playerMove == "paper" && computerMove == "scissors") {
-  console.log("Computer Wins!");
-} else if (playerMove == "paper" && computerMove == "paper") {
-  console.log("Even -- Again!");
-} else if (playerMove == "scissors" && computerMove == "paper") {
-  console.log("Player Wins!");
-} else if (playerMove == "scissors" && computerMove == "rock") {
-  console.log("Computer Wins!");
-} else if (playerMove == "scissors" && computerMove == "scissors") {
-  console.log("Even -- Again!");
+function getWinner(playerMove, computerMove) {
+  if (playerMove == "rock" && computerMove == "paper") {
+    console.log("Computer Wins!");
+  } else if (playerMove == "rock" && computerMove == "scissors") {
+    console.log("Player Wins!");
+  } else if (playerMove == "rock" && computerMove == "rock") {
+    console.log("Even -- Again!");
+  } else if (playerMove == "paper" && computerMove == "rock") {
+    console.log("Player Wins!");
+  } else if (playerMove == "paper" && computerMove == "scissors") {
+    console.log("Computer Wins!");
+  } else if (playerMove == "paper" && computerMove == "paper") {
+    console.log("Even -- Again!");
+  } else if (playerMove == "scissors" && computerMove == "paper") {
+    console.log("Player Wins!");
+  } else if (playerMove == "scissors" && computerMove == "rock") {
+    console.log("Computer Wins!");
+  } else if (playerMove == "scissors" && computerMove == "scissors") {
+    console.log("Even -- Again!");
+  }
 }
+
+let result = getWinner("rock", "paper");
