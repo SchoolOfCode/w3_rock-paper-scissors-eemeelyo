580287ed301450d9e679607d095393dafe1d23ca
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-04-20 20:14:10 +0100

    Finished Task 1

---
+++ main.js
@@ -0,0 +1,22 @@
+let playerMove = "scissors";
+let computerMove = "rock";
+
+if (playerMove == "rock" && computerMove == "paper") {
+  console.log("Player Wins!");
+} else if (playerMove == "rock" && computerMove == "scissors") {
+  console.log("Computer Wins!");
+} else if (playerMove == "rock" && computerMove == "rock") {
+  console.log("Even -- Again!");
+} else if (playerMove == "paper" && computerMove == "rock") {
+  console.log("Player Wins!");
+} else if (playerMove == "paper" && computerMove == "scissors") {
+  console.log("Computer Wins!");
+} else if (playerMove == "paper" && computerMove == "paper") {
+  console.log("Even -- Again!");
+} else if (playerMove == "scissors" && computerMove == "paper") {
+  console.log("Player Wins!");
+} else if (playerMove == "scissors" && computerMove == "rock") {
+  console.log("Computer Wins!");
+} else if (playerMove == "scissors" && computerMove == "scissors") {
+  console.log("Even -- Again!");
+}
