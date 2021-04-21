6544e5025112eb0210ed5b95fd4287e27b119a73
EmilioBC <62893436+EmilioBC@users.noreply.github.com>
2021-04-21 05:21:40 +0100

    Finished Task-5. Added while loops:to repeat the game, and confirm:whether continue or exit game.

--- main.js
+++ main.js
@@ -1,45 +1,86 @@
 // let playerMove = "scissors";
 // let computerMove = "rock";
 
-function getWinner(playerMove, computerMove) {
-  if (playerMove == "rock" && computerMove == "paper") {
-    return -1;
-  } else if (playerMove == "rock" && computerMove == "scissors") {
-    return 1;
-  } else if (playerMove == "rock" && computerMove == "rock") {
-    return 0;
-  } else if (playerMove == "paper" && computerMove == "rock") {
-    return 1;
-  } else if (playerMove == "paper" && computerMove == "scissors") {
-    return -1;
-  } else if (playerMove == "paper" && computerMove == "paper") {
-    return 0;
-  } else if (playerMove == "scissors" && computerMove == "paper") {
-    return 1;
-  } else if (playerMove == "scissors" && computerMove == "rock") {
-    return -1;
-  } else if (playerMove == "scissors" && computerMove == "scissors") {
-    return 0;
-  } else {
-    console.log("ERROR!");
-  }
-}
+let playerScore = 0;
+let computerScore = 0;
 
-function computerChoice() {
-  let randNum = Math.floor(Math.random() * 3 + 1);
-  // return randNum;
-  // console.log(rand);
-  if (randNum == 1) {
-    return "rock";
-  } else if (randNum == 2) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-}
-// console.log(computerChoice());
+let stillPlaying = true;
 
-// console.log(computerChoice);
-let playerChoice = prompt("Type rock, paper or scissors or you choice.");
-let result = getWinner(playerChoice, computerChoice());
-alert(result);
+while (stillPlaying === true) {
+  function getWinner(playerMove, computerMove) {
+    if (playerMove == "rock" && computerMove == "paper") {
+      return -1;
+    } else if (playerMove == "rock" && computerMove == "scissors") {
+      return 1;
+    } else if (playerMove == "rock" && computerMove == "rock") {
+      return 0;
+    } else if (playerMove == "paper" && computerMove == "rock") {
+      return 1;
+    } else if (playerMove == "paper" && computerMove == "scissors") {
+      return -1;
+    } else if (playerMove == "paper" && computerMove == "paper") {
+      return 0;
+    } else if (playerMove == "scissors" && computerMove == "paper") {
+      return 1;
+    } else if (playerMove == "scissors" && computerMove == "rock") {
+      return -1;
+    } else if (playerMove == "scissors" && computerMove == "scissors") {
+      return 0;
+    } else {
+      console.log("ERROR!");
+    }
+  }
+
+  function computerChoice() {
+    let randNum = Math.floor(Math.random() * 3 + 1);
+    // return randNum;
+    // console.log(rand);
+    if (randNum == 1) {
+      return "rock";
+    } else if (randNum == 2) {
+      return "paper";
+    } else {
+      return "scissors";
+    }
+  }
+  // console.log(computerChoice());
+  // console.log(computerChoice);
+
+  function playerChoice() {
+    let player = prompt(
+      "Pick a number. (1)-rock, (2)-paper or (3)-scissors of your choice."
+    );
+
+    if (player == 1) {
+      return "rock";
+    } else if (player == 2) {
+      return "paper";
+    } else if (player == 3) {
+      return "scissors";
+    } else {
+      alert("Select only numbers 1, 2 or 3");
+      return player;
+    }
+  }
+
+  let result = getWinner(playerChoice(), computerChoice());
+
+  if (result == -1) {
+    alert(result + " -You LOSE!");
+  } else if (result == 0) {
+    alert(result + " -Draw");
+  } else {
+    alert(result + " -You WIN!");
+  }
+
+  function continuePlay() {
+    let r = confirm("Continue Playing?");
+    if (r == true) {
+    } else {
+      alert("Thank you for playing! Bye!");
+      stillPlaying = false;
+    }
+  }
+
+  continuePlay();
+}
